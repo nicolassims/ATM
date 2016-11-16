1e7ecf661699e17b942c432d6a6ebd425f6c1cdb
nicolassims <sableye.nico@gmail.com>
2016-11-16 15:10:56 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * @author Sims, Nicolas (sableye.nico@gmail.com)
+ * @version 0.0.1
+ * @summary Design a program for the Hollywood Movie Rating Guide || created: 10.11.2016
+ * @todo
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+function main() {
+
+}
+
+main();
+
+function populateMovieArray() {
+    
+}
